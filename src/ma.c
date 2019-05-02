ff1d54bf9adc255403dea63500d8346fa70c57b0
mariajbp <mariajbp00@gmail.com>
2019-05-02 19:50:21 +0100

    ma

--- src/ma.c
+++ src/ma.c
@@ -1,6 +1,11 @@
 #include "ma.h" 
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
 
-
+/*
 struct artigo
 {
 	char name[LENGTH];
@@ -8,12 +13,10 @@ struct artigo
 	size_t code[7]; 
 };
 
-Artigo create_Artigo(char n[LENGTH], char d[LENGTH], double p, size_t c) //abrir ficheiro e escrever lá
+Artigo create_Artigo(char n[LENGTH], double p, size_t c) //abrir ficheiro e escrever lá
 {
 	Artigo a = malloc(sizeof(struct artigo));
-
 	memcpy(a->name,n,LENGTH);
-	memcpy(a->description,d,LENGTH);
 	a->price = p;
 	a->code = c;
 
@@ -24,8 +27,11 @@ void destroy_Artigo(Artigo a)
 {
 	free(a);
 }
+*/
 
 /**** Gets e Sets para alterar os dados *****/
+
+/*
 
 char* nameArtigo(Artigo a)
 {
@@ -47,22 +53,110 @@ void changePrice(Artigo a, double new_price)
 	a->price = new_price;
 }
 
+*/
 
-void insertArtigo(char[LENGTH] name, double price)
+int insertString(char* name, int size)
 {
-	endln = "\n"; 
-	space = " "
-	//abrir ficheiro, se não existir é criado
-	int fp = open("ARTIGOS.txt", O_CREAT | O_TRUNC | O_WRONLY, 0777);
-	//escrever em argv[2] o nome e argv[3] o preço, gerar um código consoante o numero da linha do ficheiro em que está e escreve-o 
-	write(fp,name,strlen(name));
-	write(fp,space,strlen(space));
-	write(fp,price,sizeof(double));
-	write(fp,endln,strlen(endln));
+	int ref = 0;
+	int w = -1;
+	int fd = open("strings.txt", O_CREAT | O_WRONLY, 0600);
+
+	if(fd == -1)
+	{
+		perror("Unable to open file strings");
+		exit(-1);
+	}
+	else
+	{
+		ref = lseek(fd, 0, SEEK_END);
+		w = write(fd, name, size);
+	}
+	close(fd);
+	return ref;
+}
+
+
+void insertArtigo(char* name, double price)
+{
+	int size = strlen(name);
+	int code = 0;
+	int ref = 0;
+	int w = -1;
+
+	int fd = open("artigos.txt", O_CREAT | O_RDWR , 0600);
+
+	if(fd == -1)
+	{
+		perror("Unable to open file stocks");
+		exit(-1);
+	}
+	else
+	{	
+		lseek(fd, -(2*sizeof(int)+sizeof(double)), SEEK_END);
+		read(fd, &code, sizeof(int));
+
+		code++;
+		lseek(fd, 0, SEEK_END);
+		w = write(fd, &code, sizeof(int));
+
+		ref = insertString(name, size);
+		w += write(fd, &ref, sizeof(int));
+
+		w += write(fd, &price, sizeof(double));
+	}
+	close(fd);
+}
+
+void printfArtigos(){
+	int code = 0;
+	int ref = 0;
+	double q = 0.0;
+	int r = -1;
+	int w = -1;
+
+	int fd = open("artigos.txt", O_RDONLY);
+
+
+		for(int i = 0; r != 0; i++){
+			r = read(fd, &code, sizeof(int));
+			//w = write(1, &code, sizeof(int));
+			printf("Code: %d\n", code);
+
+			r = read(fd, &ref, sizeof(int));
+			//w += write(1, &ref, sizeof(int));
+			printf("Ref: %d\n", ref);
+
+			r = read(fd, &q, sizeof(double));
+			//w += write(1, &q, sizeof(double));
+			printf("Price: %f\n", q);
+		}
+
+	 close(fd);
 
 }
 
+void printStrings(){
+
+char buff[4];
+
+	int fd = open("strings.txt", O_RDONLY);
+
+	if(fd == -1){
+		perror("Unable to open file strings");
+		exit(-1);
+	}
+	else{
+	read(fd, buff, sizeof(char)*4);
+
+	close(fd);
+
+	write(1, buff, sizeof(char)*4);
+	}
+
+}
 /**
+
+
 
 // funcao que apaga um artigo, corre o ficheiro STRINGS = mesmo nome, mesmo preco -> delete
 void deleteArtigo(char[LENGTH] name, double price)
@@ -94,7 +188,9 @@ int main(int argc, char *argv[]) //main do MA
 	}
 
 	*/
-	insertArtigo(name,price);
+	insertArtigo("asdf", 2.0);
+	printfArtigos();
+	printStrings();
 
 	return 0;
 }
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "colors.h"
+
+int main(int argc, char** argv)
+{
+	
+}
