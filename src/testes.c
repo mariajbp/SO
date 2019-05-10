55d0a50f1bcf95924890117e2d37ff453e64abb3
mimr21 <mariamiguel211999@gmail.com>
2019-05-10 18:30:24 -0400

    testes

--- src/testes.c
+++ src/testes.c
@@ -12,11 +12,17 @@ int main(int argc, char const *argv[]){
 	if(argc == 1){
 		FILE* fd = fopen("testesMA.txt", "w");
 		float f = 0.0;
+		int i , c = 0;
 
-		for(int i = 0; i < 1000000; i++){
+		for(i = 0; i < 5000; i++){
 			f++;
 			fprintf(fd, "i bola %f\n", f);
 		}
+		for(i = 0; i < 20; i++){
+			c+= 200;
+			fprintf(fd, "n %d quadrado\n", c);
+		}
+
 
 		fclose(fd);
 		printf("DONE");
@@ -25,7 +31,7 @@ int main(int argc, char const *argv[]){
 		FILE* fd = fopen("testesCV.txt", "w");
 		int f = 50;
 
-		for(int i = 1; i < 1000000; i++){
+		for(int i = 1; i < 5000; i++){
 			f++;
 			fprintf(fd, "%d %d\n", i, -f);
 		}
